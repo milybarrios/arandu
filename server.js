5a70406f9edf58c24a143719a43ad771ca817e54
Rodrigo Garcete <oneosmar89@gmail.com>
2015-11-29 14:48:35 -0300

    Modify config server.js, add mongoose

--- server.js
+++ server.js
@@ -1,5 +1,8 @@
 process.env.NODE_ENV = process.env.NODE_ENV || 'development';
 
+var mongoose = require('./config/mongoose');
+
+var db = mongoose();
 var express = require('./config/express');
 
 var app = express();
