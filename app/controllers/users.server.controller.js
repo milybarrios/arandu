2794990926dd2bd729ea1a39f6c8133c54cf0eeb
Rodrigo Garcete <oneosmar89@gmail.com>
2015-11-29 15:38:30 -0300

    Add controller for model User

---
+++ app/controllers/users.server.controller.js
@@ -0,0 +1,17 @@
+/**
+ * Created by rodrigo on 29/11/15.
+ */
+
+var User = require('mongoose').model('User');
+
+exports.create = function (req, res, next){
+    var user = new User(req.body);
+
+    user.save(function(error){
+        if (error){
+            return next(error);
+        } else {
+            res.json(user);
+        }
+    });
+};
