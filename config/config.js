cedf1669102da8158a22ad55eddd236f6817c836
Rodrigo Garcete <oneosmar89@gmail.com>
2015-11-29 14:36:09 -0300

    Moved location for major organization

---
+++ config/config.js
@@ -0,0 +1,5 @@
+/**
+ * Created by rodrigo on 29/11/15.
+ */
+
+module.exports = require('./env/' + process.env.NODE_ENV + '.js');
--- config/env/config.js
+++
@@ -1,5 +0,0 @@
-/**
- * Created by rodrigo on 29/11/15.
- */
-
-module.exports = require('./env/' + process.env.NODE_ENV + '.js');
