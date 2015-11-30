403cf85eefd7417b7afac273f78f36df738d283c
Rodrigo Garcete <oneosmar89@gmail.com>
2015-11-29 22:17:41 -0300

    Modify definition model User for add validations

--- app/models/user.server.model.js
+++ app/models/user.server.model.js
@@ -8,14 +8,33 @@ Schema = mongoose.Schema;
 var userSchema = new Schema({
     nombre: {
         type: String,
-        trim: true
+        trim: true,
+        required: true
     },
     apellido: {
         type: String,
-        trim: true
+        trim: true,
+        required: true
     },
-    email: String,
-    clave: String,
+    email: {
+        type: String,
+        unique: true,
+        index: true,
+        required: true,
+        match: /.+@.+\..+/
+    },
+    clave: {
+        type: String,
+        validate: [
+            function(clave){
+                return clave.length >= 6;
+            }, 'Clave debe tener mas de 6 digitos.'
+        ]
+    },
+    role: {
+        type: String,
+        enum: ['Admin', 'Owner', 'User']
+    },
     created: {
         type: Date,
         default: Date.now
@@ -24,6 +43,10 @@ var userSchema = new Schema({
 
 userSchema.virtual('nombreCompleto').get(function(){
     return this.nombre + ' ' + this.apellido;
+}).set(function (nombreCompleto){
+    var splitNombre = nombreCompleto.split(' ');
+    this.nombre = splitNombre[0] || '';
+    this.apellido = splitNombre[1] || '';
 });
 
 mongoose.model('User', userSchema);
